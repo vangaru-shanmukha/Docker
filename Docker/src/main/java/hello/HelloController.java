96630636bddb40b658e6aad187e73c427b266c0c
vangaru-shanmukha <vangarushanmukha@gmail.com>
2020-09-04 12:48:47 +0530

    random changes

--- Docker/src/main/java/hello/HelloController.java
+++ Docker/src/main/java/hello/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String index1() {
-        System.out.println("change16");
+        System.out.println("change17");
         return text + "hello from Shanmukha!";
     }
     
