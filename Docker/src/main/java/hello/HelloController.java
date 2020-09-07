0a46746fa9c71f9dbecf73de8a6d1c4ee49610c5
vangaru-shanmukha <vangarushanmukha@gmail.com>
2020-09-07 09:40:42 +0530

    updates jenkins file

--- Docker/src/main/java/hello/HelloController.java
+++ Docker/src/main/java/hello/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String index1() {
-        System.out.println("change24");
+        System.out.println("change25");
         return text + "hello from Shanmukha!";
     }
     
